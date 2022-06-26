dcaf2fc7bae8887c3c95dd6290d1c0e599987e11
Augusto Monteiro <augusto.monteiro@pxcreativelab.com>
2022-06-25 23:08:20 -0300

    finalizado projeto

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
 import { format, isPast } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { CheckCircle, Lock } from 'phosphor-react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classnames from 'classnames';
 
 interface LessonProps {
   title: string;
@@ -11,19 +12,36 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR,
   });
 
+  const isActiveLesson = slug === props.slug;
+
+  //rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500
+
   return (
     <>
       <Link to={`/event/lesson/${props.slug}`} className="group">
         <span className="text-gray-300">{availableDateFormatted}</span>
-        <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <div
+          className={classnames(
+            'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 relative',
+            {
+              'bg-green-500': isActiveLesson,
+            }
+          )}
+        >
           <header className="flex items-center justify-between">
             {isLessonAvailable ? (
-              <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <span
+                className={classnames('text-sm text-blue-500 font-medium flex items-center gap-2', {
+                  'text-white': isActiveLesson,
+                })}
+              >
                 <CheckCircle size={20} />
                 Conteúdo liberado
               </span>
@@ -33,11 +51,27 @@ export function Lesson(props: LessonProps) {
                 Em Breve
               </span>
             )}
-            <span className="text-xs font-bold rounded py-[0.125rem] px-2 text-white border border-green-300">
+            <span
+              className={classnames(
+                'text-xs font-bold rounded py-[0.125rem] px-2 text-white border border-green-300',
+                {
+                  'border-white': isActiveLesson,
+                }
+              )}
+            >
               {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
             </span>
           </header>
-          <strong className="text-gray-300 mt-5 block">{props.title}</strong>
+          <strong
+            className={classnames('text-gray-300 mt-5 block', {
+              'text-white': isActiveLesson,
+            })}
+          >
+            {props.title}
+          </strong>
+          {isActiveLesson && (
+            <span className="bg-green-500 w-[13px] h-[13px] absolute top-1/2 left-[-8px] -translate-y-1/2 rotate-45" />
+          )}
         </div>
       </Link>
     </>
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,50 +1,22 @@
 import { DefaultUi, Player, Youtube } from '@vime/react';
-import { gql, useQuery } from '@apollo/client';
 
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from 'phosphor-react';
 
 import '@vime/core/themes/default.css';
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        name
-        bio
-        avatarURL
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      name: string;
-      bio: string;
-      avatarURL: string;
-    };
-  };
-}
+import { useGetLessonBySlugQuery } from '../graphql/generated';
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video({ lessonSlug }: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: lessonSlug,
     },
   });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <>
         <div className="flex-1">
@@ -71,17 +43,19 @@ export function Video({ lessonSlug }: VideoProps) {
             <div className="flex-1">
               <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
               <p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
-              <div className="flex items-center gap-4 mt-6">
-                <img
-                  className="h-16 w-16 rounded-full border-2 border-blue-500"
-                  src={data.lesson.teacher.avatarURL}
-                  alt="augusto"
-                />
-                <div className="leading-relaxed">
-                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+              {data.lesson.teacher && (
+                <div className="flex items-center gap-4 mt-6">
+                  <img
+                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                    src={data.lesson.teacher.avatarURL}
+                    alt="augusto"
+                  />
+                  <div className="leading-relaxed">
+                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
             <div className="flex flex-col gap-4">
               <a
